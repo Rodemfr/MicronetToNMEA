69500decb4496669e489c9224dd668e5897bead0
Rodemfr <ronan@vendredi.homelinux.net>
2022-05-10 19:49:41 +0200

    Remove useless correction of baudrate

--- RfDriver.cpp
+++ RfDriver.cpp
@@ -40,8 +40,6 @@ bool RfDriver::Init(int gdo0Pin, MicronetMessageFifo *messageFifo, float frequen
 
 	timerInt.begin(TimerHandler);
 
-	float correctedBaudrate_kbaud = 76.8 * (MICRONET_RF_CENTER_FREQUENCY_MHZ + frequencyOffset_mHz) / MICRONET_RF_CENTER_FREQUENCY_MHZ;
-
 	cc1101Driver.Init();
 	cc1101Driver.setGDO0(gdo0Pin);
 	cc1101Driver.setCCMode(1); // set config for internal transmission mode.
@@ -51,7 +49,7 @@ bool RfDriver::Init(int gdo0Pin, MicronetMessageFifo *messageFifo, float frequen
 	cc1101Driver.setChannel(0); // Set the Channelnumber from 0 to 255. Default is cahnnel 0.
 	cc1101Driver.setChsp(199.95); // The channel spacing is multiplied by the channel number CHAN and added to the base frequency in kHz. Value from 25.39 to 405.45. Default is 199.95 kHz.
 	cc1101Driver.setRxBW(250); // Set the Receive Bandwidth in kHz. Value from 58.03 to 812.50. Default is 812.50 kHz.
-	cc1101Driver.setDRate(correctedBaudrate_kbaud); // Set the Data Rate in kBaud. Value from 0.02 to 1621.83. Default is 99.97 kBaud!
+	cc1101Driver.setDRate(MICRONET_RF_BAUDRATE_BAUD); // Set the Data Rate in kBaud. Value from 0.02 to 1621.83. Default is 99.97 kBaud!
 	cc1101Driver.setPA(12); // Set TxPower. The following settings are possible depending on the frequency band.  (-30  -20  -15  -10  -6    0    5    7    10   11   12) Default is max!
 	cc1101Driver.setSyncMode(2); // Combined sync-word qualifier mode. 0 = No preamble/sync. 1 = 16 sync word bits detected. 2 = 16/16 sync word bits detected. 3 = 30/32 sync word bits detected. 4 = No preamble/sync, carrier-sense above threshold. 5 = 15/16 + carrier-sense above threshold. 6 = 16/16 + carrier-sense above threshold. 7 = 30/32 + carrier-sense above threshold.
 	cc1101Driver.setSyncWord(0x55, 0x99); // Set sync word. Must be the same for the transmitter and receiver. (Syncword high, Syncword low)
