0432d600cdf2278276a53010f80dc1322f335b15
Rodemfr <ronan@vendredi.homelinux.net>
2022-06-19 20:42:27 +0200

    Update headers

--- CC1101Driver.h
+++ CC1101Driver.h
@@ -1,18 +1,29 @@
-/*
- ELECHOUSE_CC1101.cpp - CC1101 module library
- Copyright (c) 2010 Michael.
- Author: Michael, <www.elechouse.com>
- Version: November 12, 2010
-
- This library is designed to use CC1101/CC1100 module on Arduino platform.
- CC1101/CC1100 module is an useful wireless module.Using the functions of the
- library, you can easily send and receive data by the CC1101/CC1100 module.
- Just have fun!
- For the details, please refer to the datasheet of CC1100/CC1101.
- ----------------------------------------------------------------------------------------------------------------
- cc1101 Driver for RC Switch. Mod by Little Satan. With permission to modify and publish Wilson Shen (ELECHOUSE).
- ----------------------------------------------------------------------------------------------------------------
+/***************************************************************************
+ *                                                                         *
+ * Project:  MicronetToNMEA                                                *
+ * Purpose:  Driver for CC1101                                             *
+ * Author:   Ronan Demoment heavily based on ELECHOUSE's driver            *
+ *                                                                         *
+ ***************************************************************************
+ *   Copyright (C) 2021 by Ronan Demoment                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
  */
+
 #ifndef CC1101DRIVER_H_
 #define CC1101DRIVER_H_
 
